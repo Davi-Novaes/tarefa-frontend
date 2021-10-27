1fa094f98ee5225d48435689226ce941033b6ea9
Davi-Novaes <davirodriguessk8_@hotmail.com>
2021-10-26 21:27:09 -0300

    Alterando arquivo

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
  
 const api = axios.create({
-            baseURL: 'https://tarefa-backend-nodejs.herokuapp.com/'
+            baseURL: 'https://tarefa-backend-nodejs.herokuapp.com'
 });
 export default api;
